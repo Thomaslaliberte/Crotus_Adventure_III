49fb507e6a6287a92d78025369a4211993dd8afa
thomaslaliberte <thomas.laliberte@icloud.com>
2023-10-05 15:16:26 -0400

    preparation a vincent

--- Scripts/magicien.gd
+++ Scripts/magicien.gd
@@ -5,7 +5,7 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var gravity = 950
 var dsaut = 0
 
 func _physics_process(delta):
@@ -26,6 +26,7 @@ func _physics_process(delta):
 		velocity.x = SPEED
 		$magicien_anim.play("cour")
 		$magicien_anim.flip_h = false
+		
 	
 	if Input.is_action_pressed("gauche"):
 		velocity.x = -1*SPEED
@@ -35,6 +36,7 @@ func _physics_process(delta):
 	if Input.is_action_just_released("droite") or Input.is_action_just_released("gauche"):
 		velocity.x = 0
 		$magicien_anim.play("idle")
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	
---
+++ Scripts/mossy_niv_1.gd
@@ -0,0 +1,31 @@
+# Crotus Adventure III est un platformeur dans lequel il faut se promener au travers de nombreux niveau et tuer 
+# des ennemis dans le but d'obtenir les habiletés à battre le boss finale.
+
+#plateforme: ordinateur, format:
+
+#dans ce niveau, le personnage devra dessendre et tuer des ennemies puis, après un possible détour au feu de camp 
+#pour ce soigner, devra battre un bosse pour obtenir un double saut
+
+#la camera suivra le joueur
+
+#évenement unique: boss golem de pierre dans le coin inferieur droit
+#nouveau powerup: roulade. la recois apres avoir tuer le boss
+#
+
+
+#items génériques deja optenu:épée, glissade
+#items génériques:feu de camp, 4 ennemis, coffre
+
+
+
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
