daaba0dfda7677d22a54593edf65fe4abce23dd1
thomaslaliberte <thomas.laliberte@icloud.com>
2023-10-17 14:40:22 -0400

    ennemies

---
+++ Scripts/ennemi_champignon.gd
@@ -0,0 +1,47 @@
+extends CharacterBody2D
+
+
+const SPEED = 50.0
+const JUMP_VELOCITY = -400.0
+var alive = true
+var rng = RandomNumberGenerator.new()
+var aleatoire
+var timer = 0
+var wait_time = 2
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func move_left():
+	velocity.x = -SPEED
+	$champignon_anim.play("courir")
+	$champignon_anim.flip_h = true
+
+func move_right():
+	velocity.x = SPEED
+	$champignon_anim.play("courir")
+	$champignon_anim.flip_h = false
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	if $champignon_anim.is_playing() == false:
+		$champignon_anim.play("idle")
+	timer = timer + delta
+
+	print(aleatoire)
+	if timer > wait_time:
+		timer = 0
+		
+		aleatoire = rng.randf_range(1, 3)
+		
+		if aleatoire < 2:
+			move_left()
+		else:
+			move_right()
+	
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	move_and_slide()
--- Scripts/mossy_niv_1.gd
+++ Scripts/mossy_niv_1.gd
@@ -1,7 +1,7 @@
 # Crotus Adventure III est un platformeur dans lequel il faut se promener au travers de nombreux niveau et tuer 
 # des ennemis dans le but d'obtenir les habiletés à battre le boss finale.
 
-#plateforme: ordinateur, format:
+#plateforme: ordinateur, format:1920x1080
 
 #dans ce niveau, le personnage devra dessendre et tuer des ennemies puis, après un possible détour au feu de camp 
 #pour ce soigner, devra battre un bosse pour obtenir un double saut
@@ -14,7 +14,7 @@
 
 
 #items génériques deja optenu:épée, glissade
-#items génériques:feu de camp, 4 ennemis, coffre
+#items génériques:feu de camp, 3 champignons(ennemis), 3 yeux volant(ennemis), coffre
 
 
 
