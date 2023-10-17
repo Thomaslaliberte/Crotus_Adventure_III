2c849de7557f64ac7351541130c7cfd6b2551853
Vincent Houle <2241013@carrefour.cegepvicto.com>
2023-10-17 15:07:00 -0400

    2023-10-17 merge projet 02

---
+++ Scripts/foret_base.gd
@@ -0,0 +1,8 @@
+extends Node2D
+
+
+#Le jeu est un platformer pour l'instant avec un chevalier qui si promène.
+#à jouer sur un ordinateur pour un seul joueur
+#Le niveau est une forêt avec un grande montaigne
+#La bouteille au sommet est pour obtenir un pouvoir et le pièce sont dans la montagne en guise de décoration
+# Une fois la bouteille rammaser le joueur pourra glisser et donc passer en-dessous de la montagne
